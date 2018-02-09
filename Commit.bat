16fc9f1d0bef685554c2ea28328ccbb51e19a9c6
Tati76 <vincentfossum@hotmail.com>
2018-02-09 22:29:36 +0100

    Auto-committed on 09/02/2018

--- Commit.bat
+++ Commit.bat
@@ -5,10 +5,32 @@ REM git push -u origin master
 
 REM @echo off
  
-title Project
+REM title Project
  
-set project="C:\Users\vince\Desktop\Git Code Arduino Avion"
-set git="C:\Program Files\Git\bin" sh.exe -l
+REM set project="C:\Users\vince\Desktop\Git Code Arduino Avion"
+REM set git="C:\Program Files\Git\bin" sh.exe -l
  
-start /d %git% (cd %project% ) REM && git status && git add * && git commit -m "Auto commit" && git push -u origin master) 
+REM start /d %git% (cd %project% ) REM && git status && git add * && git commit -m "Auto commit" && git push -u origin master) 
 REM && git --no-pager log -n 5 && cmd /k cd %project%)"
+
+@echo off
+echo type "commit" or "update"
+cd "curl"
+
+set GIT_PATH="C:\Program Files\Git\bin\git.exe"
+set BRANCH = "origin"
+
+:P
+set ACTION=
+set /P ACTION=Action: %=%
+if "%ACTION%"=="c" (
+  %GIT_PATH% add -A
+	%GIT_PATH% commit -am "Auto-committed on %date%"
+	%GIT_PATH% pull %BRANCH%
+	%GIT_PATH% push %BRANCH%
+)
+if "%ACTION%"=="u" (
+	%GIT_PATH% pull %BRANCH%
+)
+if "%ACTION%"=="exit" exit /b
+goto P
