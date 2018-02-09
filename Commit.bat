4af98c1b5cf8504f4bf11d051634abd34970779c
Tati76 <vincentfossum@hotmail.com>
2018-02-09 22:25:22 +0100

    Auto commit

--- Commit.bat
+++ Commit.bat
@@ -8,7 +8,7 @@ REM @echo off
 title Project
  
 set project="C:\Users\vince\Desktop\Git Code Arduino Avion"
-set git="C:\Program Files\Git\bin" sh.exe -l -c
+set git="C:\Program Files\Git\bin" sh.exe -l
  
 start /d %git% (cd %project% ) REM && git status && git add * && git commit -m "Auto commit" && git push -u origin master) 
 REM && git --no-pager log -n 5 && cmd /k cd %project%)"
